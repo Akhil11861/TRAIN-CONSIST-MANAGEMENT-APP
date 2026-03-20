23ebde1827499ca25e937998955639eb9d95c8c5
Akhil11861 <mulla.akhilvardhan1186@gmail.com>
2026-03-20 10:32:33 +0530

    UC1

---
+++ src/TrainConsistManagementApp.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+class TrainConsistApp {
+    public static void main(String[] args) {
+
+        System.out.println("==============================================");
+        System.out.println("=== Train Consist Management App ===");
+        System.out.println("==============================================\n");
+
+        List<String> trainConsist = new ArrayList<>();
+
+        System.out.println("Train initialized successfully...");
+        System.out.println("Current number of bogies:   " + trainConsist.size());
+    }
+}
